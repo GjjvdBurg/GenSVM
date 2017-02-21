37bfe2a69505ae5d305cda2bb53a335063b0f408
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2017-02-21 18:48:33 -0500

    update docs

--- search/all_4.js
+++ search/all_4.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['elapsed_5fiter',['elapsed_iter',['../structGenModel.html#a5afc3c4fcb0f0e9f96c5b6ac9800b94a',1,'GenModel']]],
   ['epsilon',['epsilon',['../structGenModel.html#aee292fba96dcb9893f27b4eac1a00374',1,'GenModel::epsilon()'],['../structGenTask.html#a9c84db18b7bd7662afad1bce2cffd9e8',1,'GenTask::epsilon()']]],
   ['epsilons',['epsilons',['../structGenGrid.html#aad4496c2e29e20d473aa9c4d5316fe72',1,'GenGrid']]],
   ['err',['err',['../gensvm__print_8h.html#aede5746344fdce99647541101eaa7f06',1,'err(const char *fmt,...):&#160;gensvm_print.c'],['../gensvm__print_8c.html#aede5746344fdce99647541101eaa7f06',1,'err(const char *fmt,...):&#160;gensvm_print.c']]],
--- search/variables_10.js
+++ search/variables_10.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['sigma',['Sigma',['../structGenData.html#a2b2e95ac063ddcfd3d9cf184fbd671b9',1,'GenData']]],
-  ['spz',['spZ',['../structGenData.html#af2cdf64f8737cc7aacc91cff3bb11a19',1,'GenData']]]
+  ['spz',['spZ',['../structGenData.html#af2cdf64f8737cc7aacc91cff3bb11a19',1,'GenData']]],
+  ['status',['status',['../structGenModel.html#a211e350fce7739072e41fc731bc70e30',1,'GenModel']]]
 ];
