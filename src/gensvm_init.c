8f3007aa11b23d57e5a347e1fbf395027d8cd1fc
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2018-03-26 19:50:13 +0100

    Seed the model after kernel processing, so seeds can be applied with kernels.

--- src/gensvm_init.c
+++ src/gensvm_init.c
@@ -75,7 +75,7 @@ void gensvm_init_V(struct GenModel *from_model,
 
 		if (data->Z == NULL) {
 			// sparse matrix
-			visit_count = Calloc(long, m+1);
+			visit_count = Calloc(long, to_model->m+1);
 			end = ((data->spZ->type == CSR) ? data->spZ->n_row 
 					: data->spZ->n_col);
 			for (a=0; a<end; a++) {
@@ -103,10 +103,10 @@ void gensvm_init_V(struct GenModel *from_model,
 			free(visit_count);
 		} else {
 			// dense matrix
-			for (i=0; i<n; i++) {
-				for (j=0; j<m+1; j++) {
-					value = matrix_get(data->Z, n, m+1, 
-							i, j);
+			for (i=0; i<to_model->n; i++) {
+				for (j=0; j<to_model->m+1; j++) {
+					value = matrix_get(data->Z, to_model->n, 
+							to_model->m+1, i, j);
 					col_min[j] = minimum(col_min[j], value);
 					col_max[j] = maximum(col_max[j], value);
 				}
@@ -118,17 +118,19 @@ void gensvm_init_V(struct GenModel *from_model,
 			for (k=0; k<to_model->K-1; k++) {
 				rnd = ((double) rand()) / ((double) RAND_MAX);
 				value = 1.0/cmin + (1.0/cmax - 1.0/cmin)*rnd;
-				matrix_set(to_model->V, m+1, K-1, j, k, value);
+				matrix_set(to_model->V, to_model->m+1, 
+						to_model->K-1, j, k, value);
 			}
 		}
 		free(col_min);
 		free(col_max);
 	} else {
-		for (i=0; i<m+1; i++) {
-			for (j=0; j<K-1; j++) {
-				value = matrix_get(from_model->V, m+1, K-1, 
-						i, j);
-				matrix_set(to_model->V, m+1, K-1, i, j, value);
+		for (i=0; i<to_model->m+1; i++) {
+			for (j=0; j<to_model->K-1; j++) {
+				value = matrix_get(from_model->V, to_model->m+1,
+					       	to_model->K-1, i, j);
+				matrix_set(to_model->V, to_model->m+1, 
+						to_model->K-1, i, j, value);
 			}
 		}
 	}
